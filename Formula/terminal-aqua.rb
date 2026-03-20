e4b59940d8b3d8c41189e63ba3fa3f838d53802f
Dávid Török <92543505+torokdavid98@users.noreply.github.com>
2026-03-20 21:24:19 +0100

    Refactor URL assignment for CPU architecture

--- Formula/terminal-aqua.rb
+++ Formula/terminal-aqua.rb
@@ -1,34 +1,30 @@
-  class TerminalAqua < Formula
+class TerminalAqua < Formula
   desc "Track your water intake from the terminal"
   homepage "https://github.com/torokdavid98/terminal_aqua"
   version "0.1.0"
   license "MIT"
 
   on_macos do
-    if Hardware::CPU.arm ?
-    url "https://github.com/torokdavid98/terminal_aqua/releases/download/v0.1.0/aqua-aarch64-apple-d
-  arwin.tar.gz "
-  else
-    url "https://github.com/torokdavid98/terminal_aqua/releases/download/v0.1.0/aqua-x86_64-apple-da
-  rwin.tar.gz "
-  end
+    if Hardware::CPU.arm?
+      url "https://github.com/torokdavid98/terminal_aqua/releases/download/v0.1.0/aqua-aarch64-apple-darwin.tar.gz"
+    else
+      url "https://github.com/torokdavid98/terminal_aqua/releases/download/v0.1.0/aqua-x86_64-apple-darwin.tar.gz"
+    end
   end
 
   on_linux do
-    if Hardware::CPU.arm ?
-    url "https://github.com/torokdavid98/terminal_aqua/releases/download/v0.1.0/aqua-aarch64-unknown -
-    linux - gnu.tar.gz "
-  else
-    url "https://github.com/torokdavid98/terminal_aqua/releases/download/v0.1.0/aqua-x86_64-unknown-
-  linux - gnu.tar.gz "
-  end
+    if Hardware::CPU.arm?
+      url "https://github.com/torokdavid98/terminal_aqua/releases/download/v0.1.0/aqua-aarch64-unknown-linux-gnu.tar.gz"
+    else
+      url "https://github.com/torokdavid98/terminal_aqua/releases/download/v0.1.0/aqua-x86_64-unknown-linux-gnu.tar.gz"
+    end
   end
 
   def install
-  bin.install "aqua"
+    bin.install "aqua"
   end
 
   test do
     assert_match "Track your water intake", shell_output("#{bin}/aqua --help")
   end
-  end
+end
