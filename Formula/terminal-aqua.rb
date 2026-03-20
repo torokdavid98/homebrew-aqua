10189fd6ffaf905853b2949e5e735d2efd22e733
Dávid Török <92543505+torokdavid98@users.noreply.github.com>
2026-03-20 21:51:04 +0100

    terminal-aqua 0.1.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/terminal-aqua.rb
+++ Formula/terminal-aqua.rb
@@ -1,12 +1,12 @@
 class TerminalAqua < Formula
   desc "Track your water intake from the terminal"
   homepage "https://github.com/torokdavid98/terminal_aqua"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/torokdavid98/terminal_aqua/releases/download/v0.1.0/aqua-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/torokdavid98/terminal_aqua/releases/download/v0.1.1/aqua-aarch64-apple-darwin.tar.gz"
     else
       url "https://github.com/torokdavid98/terminal_aqua/releases/download/v0.1.0/aqua-x86_64-apple-darwin.tar.gz"
     end
